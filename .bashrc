2b85b2adaddfd024cd4e13dfe33a93473c3350c1
qyx22122 <tibor.macek1@gmail.com>
2024-07-13 14:50:24 +0200

    awesome bar changes

--- .bashrc
+++ .bashrc
@@ -5,7 +5,10 @@
 # If not running interactively, don't do anything
 [[ $- != *i* ]] && return
 
+clear
+neofetch
+
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
-PS1='[\u@\h \W]\$ '
+PS1='{\W} \$ '
 alias dotconfig='/usr/bin/git --git-dir=/home/tibor/dotconfig/ --work-tree=/home/tibor'
--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -203,19 +203,15 @@ awful.screen.connect_for_each_screen(function(s)
     -- Add widgets to the wibox
     s.mywibox:setup {
         layout = wibox.layout.align.horizontal,
-        { -- Left widgets
+        { -- top widgets
             layout = wibox.layout.fixed.horizontal,
-            mylauncher,
             s.mytaglist,
-            s.mypromptbox,
         },
         s.mytasklist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
-            mykeyboardlayout,
             wibox.widget.systray(),
             mytextclock,
-            s.mylayoutbox,
         },
     }
 end)
